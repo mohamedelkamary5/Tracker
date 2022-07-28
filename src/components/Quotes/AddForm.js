e909f03c195f18a88a334ec2a37991f324fef53f
Mohamed2221 <87386058+mohamedhamdy12@users.noreply.github.com>
2022-07-28 13:26:37 +0200

    done form package

--- src/components/Quotes/AddForm.js
+++ src/components/Quotes/AddForm.js
@@ -8,8 +8,11 @@ import swal from 'sweetalert';
 import { HideSlider } from '../../store/StateSlice';
 import { SendQuote } from '../../store/QuotesSlice';
 import { Button, Form, Input } from 'antd';
+import { useTranslation } from 'react-i18next';
 
 const ClintForm = () => {
+  const {t} = useTranslation()
+
   const initialState = {
     en_title: '',
     ar_title: '',
@@ -69,7 +72,7 @@ const ClintForm = () => {
           className="px-lg-5"
         >
 
-          <SliderClint title="اضافه باقه"   >
+          <SliderClint title={t("btnAddPackage")}   >
             {/* <InputCustomer values={values} setValues={setValues} /> */}
             <div className='main-input px-2'>
               <div className='row'>
@@ -77,11 +80,11 @@ const ClintForm = () => {
                 <div className='col-lg-3'>
                   <div className="mb-3">
                     <Form.Item
-                      label="الاسم بالانجليزي"
+                      label={t("en_name")}
                       name="en_nameaquote"
                       rules={[{ required: true, message: '' }]}
                     >
-                      <Input className='form-control' name='en_title' value={values.en_title} placeholder="اكتب الاسم بالانجليزي" onChange={handelChange} />
+                      <Input className='form-control' name='en_title' value={values.en_title} placeholder={t("writeNameEn")} onChange={handelChange} />
                     </Form.Item>
                     {handelError('en_title')}
                   </div>
@@ -90,11 +93,11 @@ const ClintForm = () => {
                 <div className='col-lg-3'>
                   <div className="mb-3">
                     <Form.Item
-                      label="الاسم بالعربي"
+                      label={t("ar_name")}
                       name="ar_nameaquote"
                       rules={[{ required: true, message: '' }]}
                     >
-                      <Input className='form-control' name='ar_title' value={values.ar_title} placeholder="اكتب الاسم بالعربي" onChange={handelChange} />
+                      <Input className='form-control' name='ar_title' value={values.ar_title} placeholder={t("writeNameAr")} onChange={handelChange} />
                     </Form.Item>
                     {handelError('ar_title')}
                   </div>
@@ -103,11 +106,11 @@ const ClintForm = () => {
                 <div className='col-lg-3'>
                   <div className="mb-3">
                     <Form.Item
-                      label="تسلسل"
+                      label={t("curSequence")}
                       name="sequence"
                       rules={[{ required: true, message: '' }]}
                     >
-                      <Input type="number" className='form-control' name='sequence' value={values.sequence} placeholder="اكتب تسلسل" onChange={handelChange} />
+                      <Input type="number" className='form-control' name='sequence' value={values.sequence} placeholder={t("writeCurSequence")} onChange={handelChange} />
                     </Form.Item>
                     {handelError('sequence')}
                   </div>
@@ -116,11 +119,11 @@ const ClintForm = () => {
                 <div className='col-lg-3'>
                   <div className="mb-3">
                     <Form.Item
-                      label="التكلفة"
+                      label={t("sortCost")}
                       name="cost"
                       rules={[{ required: true, message: '' }]}
                     >
-                      <Input type="number" className='form-control' name='cost' value={values.cost} placeholder="اكتب التكلفة" onChange={handelChange} />
+                      <Input type="number" className='form-control' name='cost' value={values.cost} placeholder={t("writeCost")} onChange={handelChange} />
                     </Form.Item>
                     {handelError('cost')}
                   </div>
@@ -129,11 +132,11 @@ const ClintForm = () => {
                 <div className='col-lg-3'>
                   <div className="mb-3">
                     <Form.Item
-                      label="عدد السائقين"
+                      label={t("NumDrivers")}
                       name="drivers_count"
                       rules={[{ required: true, message: '' }]}
                     >
-                      <Input type="number" className='form-control' name='drivers_count' value={values.drivers_count} placeholder="اكتب عدد السائقين" onChange={handelChange} />
+                      <Input type="number" className='form-control' name='drivers_count' value={values.drivers_count} placeholder={t("writeNumDrivers")} onChange={handelChange} />
                     </Form.Item>
                     {handelError('drivers_count')}
                   </div>
@@ -142,11 +145,11 @@ const ClintForm = () => {
                 <div className='col-lg-3'>
                   <div className="mb-3">
                     <Form.Item
-                      label="الاشهور"
+                      label={t("sortMonths")}
                       name="months"
                       rules={[{ required: true, message: '' }]}
                     >
-                      <Input type="number" className='form-control' name='months' value={values.months} placeholder="اكتب الاشهور" onChange={handelChange} />
+                      <Input type="number" className='form-control' name='months' value={values.months} placeholder={t("writeMonth")} onChange={handelChange} />
                     </Form.Item>
                     {handelError('months')}
                   </div>
@@ -163,7 +166,7 @@ const ClintForm = () => {
           <StyleFotter>
             <Button htmlType="submit" onClick={AddUser} className="btn btn-main">
               <MdPersonAddAlt className='MdPersonAddAlt' />
-              <span>اضافه باقة</span>
+              <span>{t("btnAddPackage")}</span>
             </Button>
           </StyleFotter>
         </Form>
